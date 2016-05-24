65305868a8ae1d74b9edd4b4386c5ec33450047d
Kokonoe-san <kokonoesasha@gmail.com>
2016-05-24 17:44:12 +0100

    Update tpoll.js

--- chat-plugins/tpoll.js
+++ chat-plugins/tpoll.js
@@ -5,7 +5,7 @@ const permission = 'announce';
 
 exports.commands = {
 
-        tpoll: function(target, room, user) {
+        tourpoll: function(target, room, user) {
 	             var tiers = ['Monotype', 'OU', 'Doubles OU', 'UU', 'Challenge Cup 1v1', 'Anything Goes', 'Ubers', 'Battle Factory' ];
 		          this.parse('/poll Tier of the next tournament?, ' + tiers);
 	     },
