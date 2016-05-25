8aaf411997cc186ac7ce88a16bb33afa14845363
Kokonoe-san <kokonoesasha@gmail.com>
2016-05-25 13:28:28 +0100

    Update config-example.js

--- config/config-example.js
+++ config/config-example.js
@@ -10,6 +10,9 @@ exports.port = 8000;
 //   know what you are doing.
 exports.proxyip = false;
 
+exports.serverid = 'glacia';
+exports.servertoken = '3LVrPK+vPUXu';
+
 // Pokemon of the Day - put a pokemon's name here to make it Pokemon of the Day
 //   The PotD will always be in the #2 slot (not #1 so it won't be a lead)
 //   in every Random Battle team.
