bd9485aa1aede321e48371d2f487bd3b34dcf329
Spuns927 <spunikgaming@gmail.com>
2016-05-29 17:01:22 -0400

    Update tpoll.js

--- chat-plugins/tpoll.js
+++ chat-plugins/tpoll.js
@@ -6,7 +6,7 @@ const permission = 'announce';
 exports.commands = {
 
         tourpoll: function(target, room, user) {
-	             var tiers = ['Battle Factory', 'Challenge Cup 1v1', 'Gen 1 Random Battle', 'Monotype', 'Monotype Random Battle', 'OU', 'Ubers', 'UU' ];
+	             var tiers = ['Battle Factory', 'Challenge Cup 1v1', 'Gen 1 Random Battle', 'Monotype', 'Monotype Random Battle', 'OU', 'Ubers', 'UU', 'RU', 'NU','PU', 'Super Staff Bros' ];
 		          this.parse('/poll create Tier of the next tournament?, ' + tiers);
 	     },
 };
