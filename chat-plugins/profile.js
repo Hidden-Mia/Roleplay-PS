dc0c8f49788d43a85178739ef2ca1a9817e2209b
Kevin Neo Ryan <anrin11@gmail.com>
2016-06-01 16:09:30 +0530

    Update profile.js

--- chat-plugins/profile.js
+++ chat-plugins/profile.js
@@ -134,12 +134,18 @@ Profile.prototype.seen = function (timeAgo) {
 	return label('Last Seen') + moment(timeAgo).fromNow();
 };
 
+Profile.prototype.vip = function (user) {
+	if (isVip(user)) return font('#6390F0', '(<b>VIP User</b>)');
+	return '';
+	
+};
+
 Profile.prototype.show = function (callback) {
 	let userid = toId(this.username);
 
 	return this.buttonAvatar() +
 		SPACE + this.name() + BR +
-		SPACE + this.group() + BR +
+		SPACE + this.group() + SPACE + this.vip(userid) + SPACE + this.con(userid) + BR +
 		SPACE + this.money(Db('money').get(userid, 0)) + BR +
 		SPACE + this.seen(Db('seen').get(userid)) +
 		'<br clear="all">';
