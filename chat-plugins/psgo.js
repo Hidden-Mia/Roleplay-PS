11e8236b07bc42c70d917c73b6448c68983c0b8f
Spuns927 <spunikgaming@gmail.com>
2016-06-04 17:26:36 -0400

    Update psgo.js

--- chat-plugins/psgo.js
+++ chat-plugins/psgo.js
@@ -70,7 +70,7 @@ function cacheRarity() {
 
 global.tourCard = function (tourSize, userid) {
 	if (tourSize > 32) tourSize = 32;
-	let tourRarity = tourCardRarity[Math.floor(tourSize / 4)];
+	let tourRarity = tourCardRarity[Math.floor(tourSize / 2)];
 	let cacheValue = rareCache[cleanCard.indexOf(toId(tourRarity))];
 	let card = cacheValue[Math.round(Math.random() * (cacheValue.length - 1))];
 	if (tourRarity === 'No Card') return;
