69437b722505c8e14bb8f47c5fc19054205875a9
Kokonoe-san <kokonoesasha@gmail.com>
2016-06-05 17:27:31 +0100

    Update tpoll.js

--- chat-plugins/tpoll.js
+++ chat-plugins/tpoll.js
@@ -6,7 +6,7 @@ const permission = 'announce';
 exports.commands = {
 
         tourpoll: function(target, room, user) {
-	             var tiers = ['Battle Factory', 'Challenge Cup 1v1', 'Gen 1 Random Battle', 'Monotype', 'Monotype Random Battle', 'OU', 'Ubers', 'UU', 'RU', 'NU','PU', 'Super Staff Bros' ];
+	             var tiers = ['Battle Factory', 'Challenge Cup 2v2', 'Gen 1 Random Battle', 'Haxmons', 'Monotype', 'Monotype Random Battle', 'NU', 'OU', 'RU', 'Super Staff Bros', 'Ubers', 'UU' ];
 		          this.parse('/poll create Tier of the next tournament?, ' + tiers);
 	     },
 };
