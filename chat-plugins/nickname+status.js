a7dcf16c994ca3ab7d8fdfe9f7a10ff2156b1b71
Mia Flores <flameprince338@gmail.com>
2016-06-15 15:53:33 +0530

    Update and rename nicknameandfriendcodeandstatus.js to nickname+status.js

--- chat-plugins/nickname+status.js
+++ chat-plugins/nickname+status.js
@@ -1,40 +1,23 @@
 'use strict';
-/* Friendcode commands by jd! status and nick name commands by prince sky */
+/* commands by prince sky */
 
 exports.commands = {
         setstatus: 'statusset',
 	statusset: function (target, room, user) {
 		if (!target) return this.errorReply("Invalid command. Valid commands are `/setstatus status` and `/removesatus`.");
 		let status = Tools.escapeHTML(target.trim());
-		Db('userstatus').set(toId(user), status);
+		if (status.length > 25) return this.errorReply("Titles may not be longer than 25 characters.");
+		Db('status').set(toId(user), status);
 		this.sendReply("Status set.");
 	},
 
 	removestatus: 'statusremove',
 	statusremove: function (target, room, user) {
-		if (!Db('userstatus').has(toId(user))) return this.errorReply("You do not have a status use /setstatus [status] to set your status.");
+		if (!Db('status').has(toId(user))) return this.errorReply("You do not have a status use /setstatus [status] to set your status.");
 		Db('friendcodes').delete(toId(user));
 		this.sendReply("Status removed.");
 	},
 
-	fcadd: 'friendcodeadd',
-	friendcodeadd: function (target, room, user) {
-		if (!target) return this.errorReply("Invalid command. Valid commands are `/friendcodeadd code` and `/friendcoderemove`.");
-		let fc = Tools.escapeHTML(target.trim());
-		let reg = /^\d{4}-\d{4}-\d{4}$/;
-		if (!reg.test(fc)) return this.errorReply("Invalid friend code, example: 3110-7818-5106");
-		Db('friendcodes').set(toId(user), fc);
-		this.sendReply("Friendcode set.");
-	},
-
-	fcrmv: 'friendcoderemove',
-	fcdelete: 'friendcoderemove',
-	friendcodecdelete: 'friendcoderemove',
-	friendcoderemove: function (target, room, user) {
-		if (!Db('friendcodes').has(toId(user))) return this.errorReply("You do not have a friendcode.");
-		Db('friendcodes').delete(toId(user));
-		this.sendReply('Friendcode removed.");
-	},
 	setnn: 'setnickname',
 	setnickname: function (target, room, user) {
 		if (!target) return this.errorReply("Invalid command. Valid commands are `/setnickname [nickname] ` and `/removenickname`.");
