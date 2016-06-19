64b95f6f11c3783618e89d78b4f35d83c3afdeb9
Mia Flores <flameprince338@gmail.com>
2016-06-19 09:41:06 +0530

    Update colorscmd.js

--- chat-plugins/colorscmd.js
+++ chat-plugins/colorscmd.js
@@ -8,8 +8,6 @@ exports.commands = {
         staffcolor: function(target, room, user) {
             if (!this.can('eval')) return this.sendReply('You most be leader or admin to use this command!');
             target = target.split(',');
-            if (!target[0]) return this.parse('/staffcolorhelp');
-            if (!target[1]) return this.parse('/staffcolorhelp');
             if (!target[2]) return this.parse('/staffcolorhelp');
             this.sendReply('|raw|' + user.group + '<b><font color="' + color(toId(this.user.name)) + '">' + user.name + ':&nbsp;</font></b><font color="' + target[0] + '"><font face="' + target[1] + '">' + Tools.escapeHTML(target[2]) + '</font></font>');
          },
@@ -18,9 +16,9 @@ exports.commands = {
          },
 
          color: function(target, room, user) {
-            if (!this.can('eval') && isVip(user)) return this.sendReply('You most be leader or admin to use this command!');
+            if (!this.can('eval') return this.sendReply('You most be leader or admin to use this command!');
+            if (isVip(user)) return false;
             target = target.split(',');
-            if (!target[0]) return this.parse('/colorhelp');
             if (!target[1]) return this.parse('/colorhelp');
             this.sendReply('|raw|' + user.group + '<b><font color="' + color(toId(this.user.name)) + '">' + user.name + ':&nbsp;</font></b><font color="' + target[0] + '' + Tools.escapeHTML(target[1]) + '</font>');
          },
