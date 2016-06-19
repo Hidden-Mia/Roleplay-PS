ff85b5a8c5ce67e02ad6f2317b0d10ce0ec99e28
Mia Flores <flameprince338@gmail.com>
2016-06-19 09:04:49 +0530

    Add files via upload
    Fix bug

--- chat-plugins/emoticons.js
+++ chat-plugins/emoticons.js
@@ -1,139 +1,87 @@
 'use strict';
 
-let color = require('../config/color');
+const color = require('../config/color');
+let demFeels = function () {};
+demFeels.getEmotes = function () {
+	return {};
+};
+try {
+	demFeels = require('dem-feels');
+} catch (e) {
+	console.error(e);
+}
 
 exports.parseEmoticons = parseEmoticons;
 
-let emotes = {
+// for travis build
+if (typeof demFeels.extendEmotes === 'function') {
+	// example extending emotes
+	demFeels.extendEmotes({
 	'#freewolf': 'http://i.imgur.com/ybxWXiG.png',
-	'4Head': 'https://static-cdn.jtvnw.net/emoticons/v1/354/1.0',
-	'DansGame': 'https://static-cdn.jtvnw.net/emoticons/v1/33/1.0',
-	'Doge': 'http://fc01.deviantart.net/fs71/f/2014/279/4/5/doge__by_honeybunny135-d81wk54.png',
-	'EleGiggle': 'https://static-cdn.jtvnw.net/emoticons/v1/4339/2.0',
-	'FacePalm': 'http://i.imgur.com/lv3GmpM.png',
-	'FailFish': 'https://static-cdn.jtvnw.net/emoticons/v1/360/1.0',
-	'feelsackbr': 'http://i.imgur.com/BzZJedC.jpg?1',
-	'feelsbebop': 'http://i.imgur.com/TDwC3wL.png',
+	'bomaye': 'http://i.imgur.com/cuDGGJW.gif',
+	'dedchat': 'http://i.imgur.com/W39kJHu.jpg',
+	'disarmher': 'http://i.imgur.com/y0gYGSx.gif',
 	'feelsbd': 'http://i.imgur.com/YyEdmwX.png',
-	'feelsbirb': 'http://i.imgur.com/o4KxmWe.png',
 	'feelsbm': 'http://i.imgur.com/xwfJb2z.png',
 	'feelsbn': 'http://i.imgur.com/wp51rIg.png',
-	'feelsbt': 'http://i.imgur.com/rghiV9b.png',
-	'feelschime': 'http://i.imgur.com/uIIBChH.png',
-	'feelscrazy': 'http://i.imgur.com/NiJsT5W.png',
-	'feelscool': 'http://i.imgur.com/qdGngVl.jpg?1',
-	'feelscri': 'http://i.imgur.com/QAuUW7u.jpg?1',
-	'feelscx': 'http://i.imgur.com/zRSUw2n.gif',
+	'feelscri':'http://i.imgur.com/JEMjW4f.jpg',
 	'feelsdd': 'http://i.imgur.com/fXtdLtV.png',
 	'feelsdoge': 'http://i.imgur.com/GklYWvi.png',
-	'feelsemo': 'http://i.imgur.com/FPolh5d.jpg',
-	'feelsfdra': 'http://i.imgur.com/ZIcl9Zy.jpg',
-	'feelsfro': 'http://i.imgur.com/ijJakJT.png',
-	'feelsgay': 'http://i.imgur.com/zQAacwu.png?1',
+	'feelspac': 'http://i.imgur.com/Dc9DBUz.png',
 	'feelsgd': 'http://i.imgur.com/Jf0n4BL.png',
 	'feelsgn': 'http://i.imgur.com/juJQh0J.png',
-	'feelsgro': 'http://i.imgur.com/jLhP0bZ.png?1',
-	'feelshigh': 'http://i.imgur.com/s9I2bxp.jpg?1',
-	'feelshlwn': 'http://i.imgur.com/OHMDVNJ.jpg',
 	'feelshp': 'http://i.imgur.com/1W19BDG.png',
-	'feelsho': 'http://i.imgur.com/J4oUHm2.png?1',
-	'feelsilum': 'http://i.imgur.com/CnyGTTD.png',
-	'feelsjig': 'http://i.imgur.com/hSzqy5z.png?1',
-	'feelsjpn': 'http://i.imgur.com/Zz2WrQf.jpg',
-	'feelskawaii': 'http://i.imgur.com/kLnDaYD.png', 
-	'feelsky': 'http://i.imgur.com/BtATPId.png?1',
-	'feelslelouch': 'http://i.imgur.com/qZrV75o.png',
-	'feelslot': 'http://i.imgur.com/tl88F7i.png?1',
-	'feelslu': 'http://i.imgur.com/REEBSOT.png?1',
 	'feelsmd': 'http://i.imgur.com/DJHMdSw.png',
-	'feelsmixtape': 'http://i.imgur.com/7lncwng.png',
+	'feelskink': 'http://i.imgur.com/ubnzm6w.gif',
 	'feelsnv': 'http://i.imgur.com/XF6kIdJ.png',
-	'feelsns': 'http://i.imgur.com/jYRFUYW.jpg?1',
 	'feelsok': 'http://i.imgur.com/gu3Osve.png',
-	'feelsorange': 'http://i.imgur.com/3fxXP16.jpg',
-	'feelspanda': 'http://i.imgur.com/qi7fue9.png',
-	'feelspaul': 'http://imgur.com/KuDZMJR.png',
-	'feelsshrk': 'http://i.imgur.com/amTG3jF.jpg',
 	'feelspika': 'http://i.imgur.com/mBq3BAW.png',
-	'feelsPika': 'http://i.imgur.com/eoTrTkn.png?1',
 	'feelspink': 'http://i.imgur.com/jqfB8Di.png',
 	'feelspn': 'http://i.imgur.com/wSSM6Zk.png',
-	'feelspoli': 'http://i.imgur.com/FnzhrWa.jpg?1',
-	'feelsPoli': 'http://i.imgur.com/sbKhXZE.jpg?1',
-	'feelsrb': 'http://i.imgur.com/L6ak1Uk.png',
+	'feelspr': 'http://i.imgur.com/3VtkKfJ.png',
 	'feelsrg': 'http://i.imgur.com/DsRQCsI.png',
 	'feelsrs': 'http://i.imgur.com/qGEot0R.png',
 	'feelssc': 'http://i.imgur.com/cm6oTZ1.png',
-	'feelsseis': 'http://i.imgur.com/gGGYxrE.png?1',
-	'feelsshi': 'http://i.imgur.com/VzlGZ1M.jpg',
-	'feelsslo': 'http://i.imgur.com/iQuToJf.jpg?1',
-	'feelssnake': 'http://i.imgur.com/xoJnDUD.png',
-	'feelstea': 'http://i.imgur.com/M0f2zgJ.jpg?1',
-	'feelstired': 'http://i.imgur.com/EgYViOs.jpg',
-	'feelsdrg': 'http://i.imgur.com/UZzWcA3.png',
-	'feelsvolc': 'http://i.imgur.com/QXlKzZd.png?1',
-	'feelsvpn': 'http://i.imgur.com/ODTZISl.gif',
-	'feelswin': 'http://i.imgur.com/rbs9pZG.png?1',
-	'feelswnk': 'http://i.imgur.com/K1GhJaN.png', 
+	'feelstea': 'http://i.imgur.com/XQc8yZ2.gif',
+	'Focus': 'http://i.imgur.com/dUN6xWl.gif',
 	'funnylol': 'http://i.imgur.com/SlzCghq.png',
-	'happyface': 'http://imgur.com/krzCL3j.jpg',
 	'hmmface': 'http://i.imgur.com/Z5lOwfZ.png',
-	'hypnotoad': 'http://i.imgur.com/lJtbSfl.gif',
-	'jcena': 'http://i.imgur.com/hPz30Ol.jpg?2',
-	'Kappa': 'http://i.imgur.com/ZxRU4z3.png?1',
-	'Kreygasm': 'https://static-cdn.jtvnw.net/emoticons/v1/41/1.0',
-	'meGusta': 'http://cdn.overclock.net/3/36/50x50px-ZC-369517fd_me-gusta-me-gusta-s.png',
-	'MingLee': 'https://static-cdn.jtvnw.net/emoticons/v1/68856/2.0',
+	'llamacool': 'http://i.imgur.com/X1x3728.gif',
+	'llamafood': 'http://i.imgur.com/SUZkz5p.gif',
+	'llamatea': 'http://i.imgur.com/nJnakEU.gif',
+	'llamaywn': 'http://i.imgur.com/SVj8kBt.gif',
+	'llamawave': 'http://i.imgur.com/KWAQbPu.gif',
 	'noface': 'http://i.imgur.com/H744eRE.png',
-	'Obama': 'http://i.imgur.com/rBA9M7A.png',
 	'oshet': 'http://i.imgur.com/yr5DjuZ.png',
-	'PeoplesChamp': 'http://i.imgur.com/QMiMBKe.png',
 	'Sanic': 'http://i.imgur.com/Y6etmna.png',
-	'stevo': 'http://imgur.com/Gid6Zjy.png',
-	'thumbsup': 'http://i.imgur.com/eWcFLLn.jpg',
-	'trollface': 'http://cdn.overclock.net/a/a0/50x50px-ZC-a0e3f9a7_troll-troll-face.png',
-	'trumpW': 'https://static-cdn.jtvnw.net/emoticons/v1/35218/1.0',
+	'welldone': 'http://i.imgur.com/DfKT8GL.gif',
+	'Work': 'http://i.imgur.com/C6veCvt.gif',
 	'wtfman': 'http://i.imgur.com/kwR8Re9.png',
-	'WutFace': 'https://static-cdn.jtvnw.net/emoticons/v1/28087/2.0',
 	'xaa': 'http://i.imgur.com/V728AvL.png',
-	'xoxo': 'http://orig00.deviantart.net/b49d/f/2014/220/5/3/ichigo_not_impressed_icon_by_magical_icon-d7u92zg.png',
 	'yayface': 'http://i.imgur.com/anY1jf8.png',
 	'yesface': 'http://i.imgur.com/k9YCF6K.png',
-	'youdontsay': 'http://r32.imgfast.net/users/3215/23/26/64/smiles/280467785.jpg',
-	'gudone': 'http://i.imgur.com/USkp1b9.png',
-	'feelsfloat': 'http://i.imgur.com/XKP1Kpf.gif'
-};
-
-let emotesKeys = Object.keys(emotes);
-let patterns = [];
-let metachars = /[[\]{}()*+?.\\|^$\-,&#\s]/g;
-
-for (let i in emotes) {
-	if (emotes.hasOwnProperty(i)) {
-		patterns.push('(' + i.replace(metachars, '\\$&') + ')');
-	}
+	'yousee': 'http://i.imgur.com/fjFRa6m.gif',
+	});
 }
-let patternRegex = new RegExp(patterns.join('|'), 'g');
+
+const emotes = demFeels.getEmotes();
+
+const emotesKeys = Object.keys(emotes).sort();
 
 /**
- * Parse emoticons in message.
- *
- * @param {String} message
- * @param {Object} room
- * @param {Object} user
- * @param {Boolean} pm - returns a string if it is in private messages
- * @returns {Boolean|String}
- */
+* Parse emoticons in message.
+*
+* @param {String} message
+* @param {Object} room
+* @param {Object} user
+* @param {Boolean} pm - returns a string if it is in private messages
+* @returns {Boolean|String}
+*/
 function parseEmoticons(message, room, user, pm) {
-	if (room.WarlicMode && !pm) {
-		room.add('|c|' + user.getIdentity().charAt(0) + user.name + '|' + parseWarlic(message));
-		return true;
-	}
-	if (typeof message !== 'string' || (!pm && room.disableEmoticons) && !~developers.indexOf(user.userid)) return false;
+	if (typeof message !== 'string' || (!pm && room.disableEmoticons)) return false;
 
 	let match = false;
 	let len = emotesKeys.length;
-
 
 	while (len--) {
 		if (message && message.indexOf(emotesKeys[len]) >= 0) {
@@ -143,18 +91,12 @@ function parseEmoticons(message, room, user, pm) {
 	}
 
 	if (!match) return false;
-		
-	//shadowbanroom message
-	let sbanmsg = message;
 
 	// escape HTML
 	message = Tools.escapeHTML(message);
 
 	// add emotes
-	message = message.replace(patternRegex, function (match) {
-		let emote = emotes[match];
-		return typeof emote === 'string' ? '<img src="' + emote + '" title="' + match + '" height="40" width="40" />' : match;
-	});
+	message = demFeels(message);
 
 	// __italics__
 	message = message.replace(/\_\_([^< ](?:[^<]*?[^< ])?)\_\_(?![^<]*?<\/a)/g, '<i>$1</i>');
@@ -164,25 +106,22 @@ function parseEmoticons(message, room, user, pm) {
 
 	let group = user.getIdentity().charAt(0);
 	if (room.auth) group = room.auth[user.userid] || group;
-	if(pm) group = user.group;
 
 	let style = "background:none;border:0;padding:0 5px 0 0;font-family:Verdana,Helvetica,Arial,sans-serif;font-size:9pt;cursor:pointer";
 
 	message = "<div class='chat'>" + "<small>" + group + "</small>" + "<button name='parseCommand' value='/user " + user.name + "' style='" + style + "'>" + "<b><font color='" + color(user.userid) + "'>" + user.name + ":</font></b>" + "</button><em class='mine'>" + message + "</em></div>";
 	if (pm) return message;
-	if (Users.ShadowBan.checkBanned(user)) {
-		user.sendTo(room, '|html|' + message);
-		Users.ShadowBan.addMessage(user, "To " + room, sbanmsg);
-	}
-	if (!Users.ShadowBan.checkBanned(user)) room.addRaw(message);
+
+	room.addRaw(message);
+
 	return true;
 }
 
 /**
- * Create a two column table listing emoticons.
- *
- * @return {String} emotes table
- */
+* Create a two column table listing emoticons.
+*
+* @return {String} emotes table
+*/
 function create_table() {
 	let emotes_name = Object.keys(emotes);
 	let emotes_list = [];
@@ -190,23 +129,20 @@ function create_table() {
 	let len = emotes_name.length;
 
 	for (let i = 0; i < len; i++) {
-		emotes_list.push("<td>" +
-			"<img src='" + emotes[emotes_name[i]] + "'' title='" + emotes_name[i] + "' height='40' width='40' />" +
-			emotes_name[i] + "</td>");
+		emotes_list.push("<td style='padding: 5px; box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.5) inset; border-radius: 5px;'>" + "<img src='" + emotes[emotes_name[i]] + "'' title='" + emotes_name[i] + "' height='50' width='50' style='vertical-align: middle;  padding-right: 5px;' />" + emotes_name[i] + "</td>");
 	}
 
-	let emotes_list_right = emotes_list.splice(len / 2, len / 2);
-
-	for (let i = 0; i < len / 2; i++) {
-		let emote1 = emotes_list[i],
-			emote2 = emotes_list_right[i];
-		if (emote2) {
-			emotes_group_list.push("<tr>" + emote1 + emote2 + "</tr>");
-		} else {
-			emotes_group_list.push("<tr>" + emote1 + "</tr>");
-		}
+	for (let i = 0; i < len; i += 4) {
+		let emoteOutput = [emotes_list[i], emotes_list[i + 1], emotes_list[i + 2], emotes_list[i + 3]];
+		if (i < len) emotes_group_list.push("<tr>" + emoteOutput.join('') + "</tr>");
 	}
-	return "<div class='infobox'><center><b><u>List of Emoticons</u></b></center>" + "<div class='infobox-limited'><table border='1' cellspacing='0' cellpadding='5' width='100%'>" + "<tbody>" + emotes_group_list.join("") + "</tbody>" + "</table></div></div>";
+
+	return (
+		"<div class='infobox'><center><font style='font-weight: bold; text-decoration: underline; color: #555;'>List of Emoticons</font></center>" +
+		"<div style='max-height: 300px; overflow-y: scroll; padding: 5px 0px;'><table style='background: rgba(245, 245, 245, 0.4); border: 1px solid #BBB;' width='100%'>" +
+		emotes_group_list.join("") +
+		"</table></div></div>"
+	);
 }
 
 let emotes_table = create_table();
@@ -234,31 +170,31 @@ exports.commands = {
 
 	emotes: 'emoticons',
 	emoticons: function (target, room, user) {
-        if (!this.canBroadcast()) return false;
-    	this.sendReply("|raw|" + emotes_table);
+		if (!this.runBroadcast()) return;
+		this.sendReply("|raw|" + emotes_table);
 	},
 	emoticonshelp: ["/emoticons - Get a list of emoticons."],
 
 	toggleemote: 'toggleemoticons',
 	toggleemotes: 'toggleemoticons',
 	toggleemoticons: function (target, room, user) {
-		if (!this.can('declare', null, room)) return this.errorReply("Access denied.");
+		if (!this.can('declare', null, room)) return false;
 		room.disableEmoticons = !room.disableEmoticons;
 		this.sendReply("Disallowing emoticons is set to " + room.disableEmoticons + " in this room.");
 		if (room.disableEmoticons) {
-			this.add("|raw|<div class=\"broadcast-red\" style=\"border-radius: 5px;\"><b>Emoticons are disabled!</b><br />Emoticons will not work.</div>");
+			this.add("|raw|<div class=\"broadcast-red\"><b>Emoticons are disabled!</b><br />Emoticons will not work.</div>");
 		} else {
-			this.add("|raw|<div class=\"broadcast-blue\" style=\"border-radius: 5px;\"><b>Emoticons are enabled!</b><br />Emoticons will work now.</div>");
+			this.add("|raw|<div class=\"broadcast-blue\"><b>Emoticons are enabled!</b><br />Emoticons will work now.</div>");
 		}
 	},
 	toggleemoticonshelp: ["/toggleemoticons - Toggle emoticons on or off."],
 
 	rande: 'randemote',
 	randemote: function (target, room, user) {
-                if (!this.canBroadcast()) return false;
+		if (!this.runBroadcast()) return;
 		let rng = Math.floor(Math.random() * emotesKeys.length);
 		let randomEmote = emotesKeys[rng];
-		this.sendReplyBox("<img src='" + emotes[randomEmote] + "' title='" + randomEmote + "' height='40' width='40' />");
+		this.sendReplyBox("<img src='" + emotes[randomEmote] + "' title='" + randomEmote + "' height='50' width='50' />");
 	},
-	randemotehelp: ["/randemote - Get a random emote."]
+	randemotehelp: ["/randemote - Get a random emote."],
 };
