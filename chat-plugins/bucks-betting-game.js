814f5767952475823d694efefb6f2546d02373bc
Mia Flores <flameprince338@gmail.com>
2016-06-19 10:21:06 +0530

    Update bucks-betting-game.js

--- chat-plugins/bucks-betting-game.js
+++ chat-plugins/bucks-betting-game.js
@@ -41,7 +41,7 @@ exports.commands = {
 		if (amount < 2) return this.errorReply("You don't have enough bucks for the bet.");
 
 		if (!target) return this.parse('/help bucksbet');
-                if (!this.canBroadcast()) return false;
+                if (!this.runBroadcast()) return;
 		if (room.id !== 'casino') return this.sendReply('|html|You can only start a game of bucks bet in the <button name = "send" value = "/join casino">Casino</button>');
 
 		switch (choice) {
